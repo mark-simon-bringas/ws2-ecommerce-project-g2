e578c501a902d8b9a224a8fe3e4659dce4cb6c32
Royal-Horizons-Bank <168077484+Royal-Horizons-Bank@users.noreply.github.com>
2025-08-31 00:06:34 +0800

    added designs

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 
